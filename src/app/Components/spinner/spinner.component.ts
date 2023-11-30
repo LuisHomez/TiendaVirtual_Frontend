6a1d71b3499ea4efc0da9484cba43586e14dc370
Luis Homez <luis.homez@outlook.com>
2023-11-30 11:55:29 -0500

    30/11/2023 spinner funcionando

---
+++ src/app/Components/spinner/spinner.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
+
+@Component({
+  selector: 'app-spinner',
+  templateUrl: './spinner.component.html',
+  styleUrls: ['./spinner.component.css']  
+})
+export class SpinnerComponent {}
